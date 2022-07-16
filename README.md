36332ddd96f6bb12466e84df82deae7331b9728e
NotCreative21 <seanray410@gmail.com>
2022-07-16 18:17:29 -0500

    yolo 3am coding

---
+++ README.md
@@ -0,0 +1,14 @@
+#### TimeServe
+
+simple time sync tool, I installed open-ntp and it was simply unnecessary. I dual boot windows + gnu/linux and do not need a daemon running to sync the time. I wrote this tiny tool to have a small program run on a server while a client can connect and convert the server's unix timestamp into the local time/date and set it with root permissions. I should have probably used something like Python for this since it wouldn't need this extra fat binary.
+
+Creates an HTTP endpoint to request the UNIX timestamp in ms. You can run the server portion with ./TimeServe --serve, any other argument will be seen as a server ip to connect to.
+
+
+##### Examples:
+```
+// server
+$ ./TimeServe --serve
+// sync
+# ./TimeServe 192.168.1.0
+```
---
+++ src/main.rs
@@ -0,0 +1,62 @@
+use chrono::{Datelike, Local, TimeZone, Timelike};
+use nix::unistd::Uid;
+use std::process::Command;
+use std::{
+    env,
+    error::Error,
+    time::{Duration, SystemTime, UNIX_EPOCH},
+};
+use surf::Url;
+use surf::{Client, Config};
+use tide::Request;
+
+#[async_std::main]
+async fn main() -> Result<(), Box<dyn Error>> {
+    let args: Vec<_> = env::args().collect();
+    if args.len() == 1 {
+        std::process::exit(0);
+    }
+    if !args.contains(&"--serve".to_string()) {
+        if !Uid::effective().is_root() {
+            panic!("You must run this executable with root permissions");
+        }
+        let client: Client = Config::new()
+            .set_base_url(
+                Url::parse(format!("http://{}", args[1]).as_str()).expect("invalid server"),
+            )
+            .set_timeout(Some(Duration::from_secs(5)))
+            .try_into()?;
+        let result = match client.get("/").await?.body_string().await?.parse() {
+            Ok(v) => v,
+            Err(_) => {
+                eprint!("Invalid response from server");
+                std::process::exit(1);
+            }
+        };
+        let dt = Local.timestamp_millis(result);
+        let date_string = format!(
+            "{}-{}-{} {}:{}:{}",
+            dt.year(),
+            dt.month(),
+            dt.day(),
+            dt.hour(),
+            dt.minute(),
+            dt.second()
+        );
+        Command::new("date").args(["--set", &date_string]).spawn()?;
+        std::process::exit(0);
+    }
+    let mut app = tide::new();
+    app.at("/").get(current_time);
+    app.listen("127.0.0.1:3030").await?;
+    Ok(())
+}
+
+async fn current_time(_: Request<()>) -> tide::Result {
+    Ok(SystemTime::now()
+        .duration_since(UNIX_EPOCH)
+        .expect("clock went backwards")
+        .as_millis()
+        .to_string()
+        .into())
+}
